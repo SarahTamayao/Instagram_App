94f31be75b128238d1980ddbb098c7177b6a7a61
Rutvi Shah <rutvims@fb.com>
2021-07-06 15:46:17 -0400

    launch screen added

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,25 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration){
+        configuration.applicationId = @"MS4LqGxoVKKZhn5x0fjCcqrmFXXSkGxFwc4eTIXb";
+        configuration.clientKey = @"LO1dcRIASKbKceCYNQThqpTqH52oVgr0pBe48lbv";
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+    [Parse initializeWithConfiguration:config];
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+    gameScore[@"score"] = @1337;
+    gameScore[@"playerName"] = @"Sean Plott";
+    gameScore[@"cheatMode"] = @NO;
+    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+     if (succeeded) {
+            NSLog(@"Object saved!");
+     } else {
+            NSLog(@"Error: %@", error.description);
+     }
+    }];
+    
     return YES;
 }
 
