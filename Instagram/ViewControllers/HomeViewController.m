52a05a3f171b319df079ae05a1bbddf7f02dfaa5
Rutvi Shah <rutvims@fb.com>
2021-07-07 16:19:26 -0400

    select photo from camera roll

--- Instagram/ViewControllers/HomeViewController.m
+++ Instagram/ViewControllers/HomeViewController.m
@@ -6,9 +6,14 @@
 //
 
 #import "HomeViewController.h"
+#import <Parse/Parse.h>
+#import "LoginViewController.h"
+#import "PostCell.h"
+#import "Post.h"
 
-@interface HomeViewController ()<UITableViewDelegate, UITableViewDataSource>
+@interface HomeViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *posts;
 
 @end
 
@@ -18,9 +23,48 @@
     [super viewDidLoad];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(refresh) userInfo:nil repeats:true];
     // Do any additional setup after loading the view.
 }
+- (IBAction)logoutPress:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        HomeViewController *LoginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+        [self presentViewController:LoginViewController animated:YES completion:^{
+        }];
+    }];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.posts.count;
+}
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell" forIndexPath:indexPath];
+    Post *post = self.posts[indexPath.row];
+    //cell.captionLabel.text =
+//    [cell.postImage setImage:post.image];
+    return cell;
+}
+
+- (void) refresh{
+    // construct query
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+//    [query whereKey:@"likesCount" greaterThan:@100];
+    query.limit = 20;
+
+    // fetch data asynchronously
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+        if (posts != nil) {
+            self.posts = posts;
+            NSLog(@"able to fetch posts");
+            NSLog(@"%lu", (unsigned long)posts.count);
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+}
 /*
 #pragma mark - Navigation
 
--- Instagram/Views/PostCell.h
+++ Instagram/Views/PostCell.h
@@ -6,11 +6,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "Post.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PostCell : UITableViewCell
-
+@property (weak, nonatomic) IBOutlet PFImageView *postImage;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
 @end
 
 NS_ASSUME_NONNULL_END
