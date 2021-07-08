7e2d4b3d3d113cf4f03e09b9edaa7bd474af1cc0
Rutvi Shah <rutvims@fb.com>
2021-07-08 12:27:45 -0400

    fixed bugs

--- Instagram/ViewControllers/ComposeViewController.m
+++ Instagram/ViewControllers/ComposeViewController.m
@@ -65,7 +65,7 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardegue *)segue sender:(id)sender {
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
