02a8b4fde5c6ecd02e114d6911a0eb1ffeca9326
Rutvi Shah <rutvims@fb.com>
2021-07-08 12:19:30 -0400

    fixed bugs

--- Instagram/ViewControllers/ComposeViewController.h
+++ Instagram/ViewControllers/ComposeViewController.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ComposeViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UITextView *captionLabel;
 @property (weak, nonatomic) IBOutlet UIButton *addButton;
-@property (strong, nonatomic) UIImage *postImage;
+@property (strong, nonatomic) UIImage *postImag;
 @end
 
 NS_ASSUME_NONNULL_END
